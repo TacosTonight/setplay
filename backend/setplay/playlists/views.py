f8fc83d78e8af1a9c8d9063609975e49c758a80f
TacosTonight <tonightwewillhavetacos@gmail.com>
2021-12-21 17:18:43 -0800

    added index view and turned songdata into module

--- backend/setplay/playlists/views.py
+++ backend/setplay/playlists/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from songdata.setlist import Setlist
+
+
+def index(request):
+    artist = "Stray From The Path"
+    sets = Setlist(artist)
+    mbid = sets.get_mbid()
+    setlists = sets.get_setlist(mbid,4)
+    most_recent = sets.most_recent_set(setlists)
+    songlist = sets.songlist(most_recent)
+   
+    return render(request, 'playlists/index.html', {'artist':artist, 'songlist': songlist})
