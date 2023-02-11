197ab8f37d33d3e7336b16ca4c63d5d72049afa8
TacosTonight <tonightwewillhavetacos@gmail.com>
2023-02-11 13:45:11 -0800

    added urls and auth and callback views with dummy data

--- backend/spotify/views.py
+++ backend/spotify/views.py
@@ -1,3 +1,28 @@
+from requests import Request, post
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+class AuthURL(APIView):
+    def get(self, request):
+        scopes = ''
+        url = Request('GET', 'https://accounts.spotify.com/authorize', params={
+            'client_id':'9d938743bfbe4d4ab59050f1ec3b2494',
+            'response_type':'code',
+            'redirect_uri':'http://127.0.0.1:8000/spotify/',
+            'scopes': 'scopes'
+        }).prepare().url
+        return Response({'url':url}, status=status.HTTP_200_OK)
+
+def spotify_callback(request):
+    code = request.GET.get('code')
+    error = request.GET.get('error')
+
+    response = post('https://accounts.spotify.com/api/token', data={
+        'grant_type': 'authorization_code',
+        'code': code,
+        'redirect_uri': 'adf',
+        'client_id': 'asdf',
+        'client_secret': 'adsf'
+    }).json()
