caa466f94e527c676f2656049abfbaa163c56d4e
TacosTonight <tonightwewillhavetacos@gmail.com>
2023-02-11 21:37:25 -0800

    added auth flow

--- backend/spotify/urls.py
+++ backend/spotify/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('get-auth-url', AuthURL.as_view(), name='spotify-auth')
+    path('get-auth-url', AuthURL.as_view(), name='spotify-auth'),
+    path('auth-confirmation', auth_confirmation, name='auth-confirmation'),
+    path('auth', authenticate, name='auth'),
+    path('callback', spotify_callback, name='callback')
 ]
--- backend/spotify/views.py
+++ backend/spotify/views.py
@@ -1,17 +1,23 @@
+import requests
 from requests import Request, post
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.http import HttpResponse
+
+REDIRECT_URI = ''
+CLIENT_ID = ''
+CLIENT_SECRET = ''
+SCOPES = ''
 
 class AuthURL(APIView):
     def get(self, request):
-        scopes = ''
         url = Request('GET', 'https://accounts.spotify.com/authorize', params={
-            'client_id':'9d938743bfbe4d4ab59050f1ec3b2494',
+            'client_id': CLIENT_ID,
             'response_type':'code',
-            'redirect_uri':'http://127.0.0.1:8000/spotify/',
-            'scopes': 'scopes'
+            'redirect_uri': REDIRECT_URI,
+            'scopes': SCOPES
         }).prepare().url
         return Response({'url':url}, status=status.HTTP_200_OK)
 
@@ -22,7 +28,24 @@ def spotify_callback(request):
     response = post('https://accounts.spotify.com/api/token', data={
         'grant_type': 'authorization_code',
         'code': code,
-        'redirect_uri': 'adf',
-        'client_id': 'asdf',
-        'client_secret': 'adsf'
+        'redirect_uri': REDIRECT_URI,
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET
     }).json()
+
+    access_token = response.get('access_token')
+    token_type = response.get('token_type')
+    refresh_token = response.get('refresh_token')
+    expires_in = response.get('expires_in')
+    error = response.get('error')
+    return redirect('auth-confirmation')
+
+def auth_confirmation(request):
+    return HttpResponse("You have been authenticated!")
+
+def authenticate(request):
+    if request.method == 'POST':
+        r = requests.get('http://127.0.0.1:8000/spotify/get-auth-url')
+        auth_url = r.json().get('url')
+        return redirect(auth_url)
+    return render (request, 'spotify/auth.html')
