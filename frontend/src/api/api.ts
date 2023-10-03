a7807b5376a8a0483c1ddfe0becbeec9c08205fd
TacosTonight <tonightwewillhavetacos@gmail.com>
2023-10-02 22:29:35 -0700

    exampleData and reactQuery

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { ArtistMatches } from "../types";
+import { ArtistMatches, Setlist } from "../types";
 
 const BASE_URL = "";
 
@@ -18,4 +18,47 @@ export const fetchArtists = async (
   } catch (error) {
     throw error;
   }
+};
+
+const exampleArtistMatches: ArtistMatches = {
+  artists: [
+    {
+      name: "Artist 1",
+      imgUrl: "/demoimage.jpeg",
+    },
+    {
+      name: "Artist 2",
+      imgUrl: "/demoimage.jpeg",
+    },
+  ],
+};
+
+export const simulateFetchArtists = async (_artistInput:string): Promise<ArtistMatches> => {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return exampleArtistMatches;
+};
+
+
+const exampleSetlist: Setlist = {
+  songs: [
+    {
+      title: "Song 1",
+      albumName: "Album 1",
+      albumArt: "https://example.com/album1.jpg",
+      duration: 240000,
+      uri: "spotify:track:123456789",
+    },
+    {
+      title: "Song 2",
+      albumName: "Album 2",
+      albumArt: "https://example.com/album2.jpg",
+      duration: 180000,
+      uri: "spotify:track:987654321",
+    },
+  ],
+};
+
+export const simulateFetchSetlist = async (_selectedArtist:string): Promise<Setlist> => {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return exampleSetlist;
 };
