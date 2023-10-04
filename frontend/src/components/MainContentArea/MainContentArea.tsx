872a880c76f12d030c5c90f8ad842af659061059
TacosTonight <tonightwewillhavetacos@gmail.com>
2023-10-04 10:24:43 -0700

    aligned setlistandstats

--- frontend/src/components/MainContentArea/MainContentArea.tsx
+++ frontend/src/components/MainContentArea/MainContentArea.tsx
@@ -4,7 +4,13 @@ import { Grid } from "@mui/material";
 
 const MainContentArea = () => {
   return (
-    <Grid container justifyContent="space-between" sx={{ height: "100%" }}>
+    <Grid
+      container
+      justifyContent="space-between"
+      sx={{ height: "100%" }}
+      alignItems="center"
+      columnSpacing={-10}
+    >
       <Grid item sx={{ flex: 1, overflowY: "auto" }}>
         <Setlist />
       </Grid>
