108f03b6b82db19399f8b535497bccc54570a538
TacosTonight <tonightwewillhavetacos@gmail.com>
2023-10-03 22:16:39 -0700

    main app area layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,22 @@
 import SearchBar from "./components/SearchBar";
+import Setlist from "./components/Setlist";
+import SetlistStats from "./components/SetlistStats";
+import { Grid } from "@mui/material";
 
 function App() {
   // Apply custom CSS to remove margin from body
   document.body.style.margin = "0";
   return (
     <>
-      <SearchBar></SearchBar>
+      <SearchBar />
+      <Grid container justifyContent="space-between" sx={{ height: "100%" }}>
+        <Grid item sx={{ flex: 1, overflowY: "auto" }}>
+          <Setlist />
+        </Grid>
+        <Grid item>
+          <SetlistStats />
+        </Grid>
+      </Grid>
     </>
   );
 }
--- frontend/src/components/Setlist/Setlist.tsx
+++ frontend/src/components/Setlist/Setlist.tsx
@@ -16,7 +16,7 @@ const Setlist = () => {
     </ListItem>
   ));
 
-  return <List>{renderedSongs}</List>;
+  return <List sx={{ height: "90vh" }}>{renderedSongs}</List>;
 };
 
 export default Setlist;
