e1539279a6a721b0357e6cf1526a012f2412e853
TacosTonight <tonightwewillhavetacos@gmail.com>
2023-10-11 23:46:29 -0700

    spotify client auth

---
+++ backend/spotify/spotify_client_authentication.py
@@ -0,0 +1,36 @@
+from django.utils import timezone
+from utils import get_spotify_token_response, calculate_token_expiration_time
+
+
+class SpotifyClientAuthentication:
+    def __init__(self, client_id, client_secret):
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.access_token, self.expires_in = self.request_new_tokens()
+
+    def request_new_tokens(self):
+        data = {
+            "grant_type": "client_credentials",
+            "client_id": self.client_id,
+            "client_secret": self.client_secret,
+        }
+
+        response = get_spotify_token_response(
+            "https://accounts.spotify.com/api/token", data=data
+        )
+        return self.parse_spotify_auth(response)
+
+    def parse_spotify_auth(self, response):
+        access_token = response.get("access_token")
+        expires_in = response.get("expires_in")
+
+        return {
+            "access_token": access_token,
+            "expires_in": calculate_token_expiration_time(expires_in),
+        }
+
+    def is_authenticated(self):
+        return self.expires_in <= timezone.now()
+
+    def refresh_tokens(self):
+        self.access_token, self.expires_in = self.request_new_tokens()
